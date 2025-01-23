cfcb4242f153811422112a6237fd68c3437e3f3c
Warpstar10010001 <alpay.gueruez@hsrw.org>
2025-01-23 02:38:51 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,48 @@
+//Boo!!
+
+import java.util.Scanner;
+
+public class Main {
+    //Global objects:
+    static Scanner scanner = new Scanner(System.in);
+
+    //Global variables:
+    static final String LASTUPDATE = "23.01.25";
+    static final String BUILD = "0001";
+    static final String DIVIDER = "\n--------------------------------------------\n";
+
+    public static void main(String[] args){
+        System.out.println("++++ NoHardFeelings ++++");
+        System.out.println("Build: " + BUILD + " / Last Updated: " + LASTUPDATE);
+        System.out.println(DIVIDER);
+
+        start();
+    }
+
+    public static void start(){
+        System.out.print("Open settings panel? [1/0]: ");
+        switch (scanner.nextInt()){
+            case 1 -> config();
+            case 0 -> {}
+            default -> exit(true, 1);
+        }
+    }
+
+    public static void config(){
+        //Change global settings stored in Settings.java
+    }
+
+    public static void exit(boolean isError, int errorCode){
+        if(isError){
+            String error = "\nError: ";
+            switch (errorCode){
+                case 1 -> error += "Invalid selection.";
+                default -> error += "Internal system conflict";
+            }
+            System.out.print(error);
+        } else {
+            System.out.println("\nSession terminated.");
+        }
+        System.exit(0);
+    }
+}

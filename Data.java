b029af7f771d3768e574f0b1221d27011f623d7d
Warpstar10010001 <alpay.gueruez@hsrw.org>
2025-01-23 02:38:44 +0100

    Create Data.java

---
+++ Data.java
@@ -0,0 +1,9 @@
+//Boo!!
+
+public class Data {
+    //Stores the position of the players of each team [0-43]
+    private static int[] playerPos = new int[16];
+    //Stores availability of every player
+    private static boolean[] playerAvailability = new boolean[16];
+
+}

6ff738bda06bec17bf55a8237aeb8ba4858eba2e
Warpstar10010001 <alpay.gueruez@hsrw.org>
2025-01-23 02:38:53 +0100

    Create Settings.java

---
+++ Settings.java
@@ -0,0 +1,10 @@
+//Boo!!
+
+public class Settings {
+    //Amount of players [1-4]
+    private static int playerCount;
+    //Player with the first move [1-4]
+    private static int playerStart;
+    //Amount of figures of every player [1-4]
+    private static int figureAmount;
+}

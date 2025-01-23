70df015a7a1a0da6d6224b2ada994697f1640804
Warpstar10010001 <alpay.gueruez@hsrw.org>
2025-01-23 02:38:50 +0100

    Create Figure.java

---
+++ Figure.java
@@ -0,0 +1,10 @@
+//Boo!!
+
+public class Figure {
+    //Number of the team
+    private int team;
+    //Player number [1-4], identifies the figure in each team
+    private int figureNum;
+    //Checks if figure placed
+    private boolean placed = false;
+}
